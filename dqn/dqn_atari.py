gradient vanishing problem fixed seemingly by removing RMSProp momentum; debug-train run for 100k steps

dqn/dqn_atari.py
@@ -44,11 +44,11 @@ def force_delete(fpath):
 class DQNEnvironment:
 
     def __init__(self, env_name="Breakout-v0", root_dir='atari_games', flicker_buffer_size=2, sample_freq=4,
-                 replay_buffer_size=1000000, history_size=4, num_train_steps=1000000,
+                 replay_buffer_size=1000000, history_size=4, num_train_steps=100000,
                  batch_size=32, viz=True, sync_freq=10000, replay_start_size=50000, viz_fps=60, num_plot_points=100,
                  episodic_reward_ema_alpha=.7, discount_factor=.99, replay_memory_cache_fname='training_cache.db',
                  video_prefix='shm_dqn', run_dir_prefix='run', print_loss_every_n_steps=50, render=False,
-                 max_replay_buffer_inmemory_size=50000, experience_db_sample_frac=.5,
+                 max_replay_buffer_inmemory_size=20000, experience_db_sample_frac=.5,
                  refresh_replay_cache_every_n_experiences=900, write_video_every_n_frames=36000):
         self.env_name = env_name
         self.root_dir = root_dir
@@ -58,6 +58,7 @@ class DQNEnvironment:
         self.refresh_replay_cache_every_n_experiences = refresh_replay_cache_every_n_experiences
         self.max_replay_buffer_inmemory_size = max_replay_buffer_inmemory_size
         self.num_db_sampled_experiences = 0
+        self.num_train_steps = num_train_steps
         self.experience_db_sample_frac = experience_db_sample_frac
         self.n_db_sample = int(self.experience_db_sample_frac * self.max_replay_buffer_inmemory_size)
         self.render = render
@@ -94,15 +95,14 @@ class DQNEnvironment:
 
         self.video_out_fpath_prefix = self.video_out_dir + os.sep + video_prefix + '-' + self.env_name + '-'
         self.dqn_constant = DQN(num_classes=self.env.action_space.n, model_folder=self.curr_models_dir,
-                                model_prefix=self.env_name)
+                                model_prefix=self.env_name, num_train_steps=self.num_train_steps)
         self.dqn_action = DQN(num_classes=self.env.action_space.n, model_folder=self.curr_models_dir,
-                              model_prefix=self.env_name)
+                              model_prefix=self.env_name, num_train_steps=self.num_train_steps)
         self.frame_count = 0
         self.curr_train_step = 0
         self.flicker_buffer_size = flicker_buffer_size
         self.history_size = history_size
         self.sample_freq = sample_freq
-        self.num_train_steps = num_train_steps
         self.replay_buffer_size = replay_buffer_size
         self.random_action_prob = 1.
         self.replay_buffer_inmemory = []
@@ -227,13 +227,15 @@ class DQNEnvironment:
             for dbk in db_keys:
                 _, _, _, _, _, train_step, frame_count, _, total_episode_ema_reward, curr_episode_reward, \
                 best_episode_reward, loss, l2_loss = self.replay_buffer_db[dbk]
-                self.plot_frame_indices.append(frame_count)
-                self.curr_episode_rewards.append(curr_episode_reward)
-                self.best_episode_rewards.append(best_episode_reward)
-                self.ema_episode_rewards.append(total_episode_ema_reward)
-                self.plot_loss_train_steps.append(train_step)
-                self.plot_losses.append(loss)
-                self.plot_l2_losses.append(l2_loss)
+                if frame_count < self.frame_count:
+                    self.plot_frame_indices.append(frame_count)
+                    self.curr_episode_rewards.append(curr_episode_reward)
+                    self.best_episode_rewards.append(best_episode_reward)
+                    self.ema_episode_rewards.append(total_episode_ema_reward)
+                    if frame_count > self.replay_start_size and train_step <= self.curr_train_step:
+                        self.plot_loss_train_steps.append(train_step)
+                        self.plot_losses.append(loss)
+                        self.plot_l2_losses.append(l2_loss)
 
             if self.frame_count >= self.replay_start_size:
                 self.random_exploration_active = False
@@ -324,10 +326,9 @@ class DQNEnvironment:
                           np.clip(self.num_plot_points, None,
                                   len(self.plot_loss_train_steps)).astype(np.int)).astype(np.int)
         loss_indices = np.array(self.plot_loss_train_steps)[idx]
-        losses = np.clip(np.array(self.plot_losses)[idx], None, 1e-5)
-        # l2_losses = np.clip(np.array(self.plot_l2_losses)[idx], None, .1)
+        losses = np.array(self.plot_losses)[idx]
+        losses = np.clip(losses, None, .0007)
         plt.plot(loss_indices, losses, '-', label='Loss')
-        # plt.plot(loss_indices, l2_losses, '-', label='L2 Loss')
         plt.legend(loc='best')
         plt.xlabel('Train Step')
         plt.ylabel('MSE Loss')
@@ -383,14 +384,17 @@ class DQNEnvironment:
         if refresh_inememory_experiences:
             self.replay_buffer_inmemory = []
             db_experience_indices = list(map(int, list(self.replay_buffer_db.keys())))
-            self.num_db_sampled_experiences = min(len(db_experience_indices), self.n_db_sample)
-            print('Sampling random', self.num_db_sampled_experiences, 'experiences from', len(db_experience_indices),
+            num_db_sampled_experiences = min(len(db_experience_indices), self.n_db_sample)
+            print('Sampling random', num_db_sampled_experiences, 'experiences from', len(db_experience_indices),
                   'experiences stored on disk and loading them to memory...')
             selected_db_indices = sorted(map(int, np.random.choice(db_experience_indices,
-                                                                   self.num_db_sampled_experiences, replace=False)))
+                                                                   num_db_sampled_experiences, replace=False)))
             for i in tqdm(range(len(selected_db_indices)), position=0, leave=True):
                 experience = [selected_db_indices[i]] + self.replay_buffer_db[str(selected_db_indices[i])]
-                self.replay_buffer_inmemory.append(experience)
+                if experience[7] <= self.frame_count:
+                    self.replay_buffer_inmemory.append(experience)
+            self.num_db_sampled_experiences = len(self.replay_buffer_inmemory)
+            print('Sampled', self.num_db_sampled_experiences, 'experiences from DB')
         self.replay_buffer_db.close()
         if save_model and not self.random_exploration_active:
             self.dqn_action.save(str(round(self.total_episode_ema_reward, 2)))

dqn/nn.py
@@ -23,9 +23,9 @@ import cv2
 class DQN:
 
     def __init__(self, num_classes, im_w=84, im_h=84, compute_bn_mean_var=True, start_step=0, dropout_enabled=False,
-                 learn_rate=2.5e-4, l2_regularizer_coeff=1e-2, num_steps=1000000, dropout_rate=.3, discount_factor=.99,
-                 update_batchnorm_means_vars=True, optimized_inference=False, load_training_vars=True,
-                 model_folder=None, model_prefix='model'):
+                 learn_rate=2.5e-4, l2_regularizer_coeff=1e-2, num_train_steps=1000000, dropout_rate=.3,
+                 discount_factor=.99, update_batchnorm_means_vars=True, optimized_inference=False,
+                 load_training_vars=True, model_folder=None, model_prefix='model'):
         if model_folder is None:
             self.model_folder = 'all_trained_models/trained_models'
         else:
@@ -37,6 +37,7 @@ class DQN:
         self.num_classes = num_classes
         self.im_h = im_h
         self.im_w = im_w
+        self.num_train_steps = num_train_steps
         self.compute_bn_mean_var = compute_bn_mean_var
         self.optimized_inference = optimized_inference
         self.x_tensor = tf.placeholder(tf.float32, shape=[None, self.im_h, self.im_w, 4],
@@ -78,12 +79,14 @@ class DQN:
 
         self.q_pred = tf.reduce_sum(self.y_pred * tf.one_hot(self.actions_tensor, self.num_classes), axis=1)
 
-        self.loss_op = tf.reduce_mean(tf.math.squared_difference(self.q_pred, self.y_gt))
+        self.loss_op = tf.math.squared_difference(self.q_pred, self.y_gt)
+        self.learn_rate_tf = tf.train.polynomial_decay(self.learn_rate_tf, global_step=self.step_ph,
+                                                       decay_steps=self.num_train_steps, end_learning_rate=1e-6)
 
         l2_losses = [self.l2_regularizer_coeff * tf.nn.l2_loss(v) for v in self.trainable_vars]
         self.loss = tf.reduce_mean(self.loss_op)
         self.reduced_loss = self.loss + tf.add_n(l2_losses)
-        self.opt = tf.train.RMSPropOptimizer(learning_rate=self.learn_rate_tf, momentum=.95)
+        self.opt = tf.train.RMSPropOptimizer(learning_rate=self.learn_rate_tf, decay=.95)
         self.grads_and_vars_tensor = self.opt.compute_gradients(self.loss, var_list=self.trainable_vars)
         self.grads_tensor = [gv[0] for gv in self.grads_and_vars_tensor if gv[0] is not None]
         self.train_op = self.opt.apply_gradients(self.grads_and_vars_tensor, global_step=self.step_ph,
@@ -214,12 +217,16 @@ class DQN:
                                                                  self.actions_tensor: actions,
                                                                  self.dropout_rate_tensor: self.dropout_rate})
         else:
-            l2_loss, loss, _, step_tf, grads, q_pred = self.sess.run([self.reduced_loss, self.loss, self.train_op,
-                                                                      self.step_ph, self.grads_tensor, self.q_pred],
-                                                             feed_dict={self.x_tensor: x,
-                                                                        self.y_gt: y,
-                                                                        self.actions_tensor: actions})
-        self.step = step_tf
+            l2_loss, loss, _, step_tf, \
+            grads, q_pred, lr = self.sess.run([self.reduced_loss, self.loss, self.train_op,
+                                               self.step_ph, self.grads_tensor, self.q_pred,
+                                               self.learn_rate_tf],
+                                              feed_dict={self.x_tensor: x,
+                                                         self.y_gt: y,
+                                                         self.actions_tensor: actions})
+        loss_np = np.mean(np.square(np.array(y) - np.array(q_pred)))
+        gm = np.mean([np.abs(g).mean() for g in grads])
+        self.step = step_tf  # Gradients vanishing before train step 221
         return l2_loss, loss, step_tf
 
     def conv_block(self, x_in, output_filters, kernel_size=3, kernel_stride=1, dilation=1, padding="VALID",